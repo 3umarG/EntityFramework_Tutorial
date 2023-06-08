d4b6115011490f97c811c3d02c24c2e600e62740
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-08 05:51:08 +0200

    Introduction to Lazy Loading Techniques

--- WF_From_DB/Form1.Designer.cs
+++ WF_From_DB/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            gvProducts = new DataGridView();
+            btnLoad = new Button();
+            ((System.ComponentModel.ISupportInitialize)gvProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // gvProducts
+            // 
+            gvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gvProducts.Dock = DockStyle.Fill;
+            gvProducts.Location = new Point(0, 0);
+            gvProducts.Name = "gvProducts";
+            gvProducts.RowTemplate.Height = 25;
+            gvProducts.Size = new Size(800, 450);
+            gvProducts.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(370, 308);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLoad);
+            Controls.Add(gvProducts);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)gvProducts).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView gvProducts;
+        private Button btnLoad;
     }
 }
--- WF_From_DB/Form1.cs
+++ WF_From_DB/Form1.cs
@@ -1,10 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WF_From_DB
 {
     public partial class Form1 : Form
     {
+        private NorthwindContext context = new();
         public Form1()
         {
             InitializeComponent();
+
+            this.FormClosed += (sender, e) => context.Dispose();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            #region Lazy Loading different approachs
+            // the first way 
+            /*
+            context.Products.Load(); // Load : load the data from Table into Local Copy 
+            this.gvProducts.DataSource = context.Products.Local.ToBindingList();
+            */
+
+            // the second way : Lazy Loading , all information about Categorie is NULL because i don't determine the Categorie in Select statement
+            //var result = (from P in context.Products
+            //             select P).ToList();
+
+            // I continue using Lazy loading , but I generate an except for this select to join with Categorie Table
+            var result = (from P in context.Products.Include(P => P.Category) select P).ToList();
+
+            // or I can choose the CategorieID in the select 
+            //var result = (from P in context.Products
+            //              select new { P.ProductID, P.ProductName, P.CategoryID }).ToList();
+
+            this.gvProducts.DataSource = result;
+
+            #endregion
+        }
+
     }
 }

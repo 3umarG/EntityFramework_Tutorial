bca9cd89345928efa19c87c8ad188baf4022639b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-09 21:31:47 +0200

    Mapping Ways Project Initial with First Migration

---
+++ Mapping&Migration02/Context/CompanyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping_Migration02.Context
+{
+    public class CompanyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlServer("Data Source=DESKTOP-EF44UM4\\SQLEXPRESS;Initial Catalog=CompanyEF;Integrated Security=True;TrustServerCertificate=True");
+    }
+}

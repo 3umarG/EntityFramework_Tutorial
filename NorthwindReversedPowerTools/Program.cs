8ded5366e99d463c7453600ebe37d7a45f5deccd
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-15 02:44:45 +0200

    Power Tools with Sotred Procedure and Views Reversed Engineering

---
+++ NorthwindReversedPowerTools/Program.cs
@@ -0,0 +1,29 @@
+﻿using NorthwindReversedPowerTools.Context;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        OutputParameter<int?> outputParameter = new();
+
+        using (NorthwindContext context = new NorthwindContext())
+        {
+            var Procedures = new NorthwindContextProcedures(context);
+
+            var topIDs =  await Procedures.GetTopIDsAsync(10, outputParameter);
+
+
+            // the code after await line will be suspended until the await returns its value ...
+            Console.WriteLine("Do Some Work Independently until the Async Function finish its work .......");
+
+           
+            foreach (var id in topIDs)
+            {
+                await Console.Out.WriteLineAsync(id.ToString());
+            }
+            await Console.Out.WriteLineAsync("\n");
+            await Console.Out.WriteLineAsync(outputParameter!.Value.ToString());
+
+        }
+    }
+}
